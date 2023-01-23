670cc3f14b08fc9d7062741237455cf6aac86be3
Carlos Carvalho <carlos@chcdc.com.br>
2023-01-23 20:46:57 -0300

    chore:Rename file app.py to main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def show_address():
     ip_address = request.remote_addr
     host = request.host
     return "It's working!\nYou are " + ip_address + "\nI'm " + host + "\n"
 
+
 if __name__ == '__main__':
     app.run()
+
